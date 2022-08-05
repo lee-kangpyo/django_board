218834584a05cb4ee0cbe8e3991dfb8c83df0a9b
MSI02 <darkzhon@hanmail.net>
2022-08-05 17:28:02 +0900

    장고 관리자 사용 및 조회, 템플릿

--- pybo/views.py
+++ pybo/views.py
@@ -1,8 +1,18 @@
-from django.shortcuts import render
-
 # Create your views here.
 
-from django.http import HttpResponse
+# from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from .models import Question
+
 
 def index(request):
-    return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    #return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    question_list = Question.objects.order_by("-create_date")
+    obj = {'question_list':question_list}
+    return render(request, 'pybo/question_list.html', obj)
+
+
+def detail(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    obj = {"question":question}
+    return render(request, 'pybo/question_detail.html', obj)
