17f057886a0528bdfcb4b73e0ae60c8f46f23b7b
MSI02 <darkzhon@hanmail.net>
2022-08-24 10:29:15 +0900

    데이터 저장하기

--- pybo/views.py
+++ pybo/views.py
@@ -1,12 +1,12 @@
-# Create your views here.
-
 # from django.http import HttpResponse
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
 from .models import Question
 
 
 def index(request):
-    #return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
+    # 단순하게 문자열을 브라우저에 출력
+    # return HttpResponse("안녕하세요 pybo에 오신것을 환영합니다.")
     question_list = Question.objects.order_by("-create_date")
     obj = {'question_list':question_list}
     return render(request, 'pybo/question_list.html', obj)
@@ -16,3 +16,14 @@ def detail(request, question_id):
     question = get_object_or_404(Question, id=question_id)
     obj = {"question":question}
     return render(request, 'pybo/question_detail.html', obj)
+
+
+def answer_create(request, question_id):
+    question = get_object_or_404(Question, id=question_id)
+    question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
+    
+    # 아래코드로 저장도 가능 Answer 클래스 import 필요
+    # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
+    # answer.save()
+    
+    return redirect("pybo:detail", question_id=question.id)
