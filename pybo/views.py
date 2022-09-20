075d1adef90f469f377dae6dbc9799fdbb341ef5
MSI02 <darkzhon@hanmail.net>
2022-09-20 17:47:08 +0900

    form으로 질문 생성하기

--- pybo/views.py
+++ pybo/views.py
@@ -2,6 +2,7 @@
 from django.shortcuts import render, get_object_or_404, redirect
 from django.utils import timezone
 from .models import Question
+from .forms import QuestionForm
 
 
 def index(request):
@@ -21,9 +22,25 @@ def detail(request, question_id):
 def answer_create(request, question_id):
     question = get_object_or_404(Question, id=question_id)
     question.answer_set.create(content=request.POST.get('content'), create_date=timezone.now())
-    
     # 아래코드로 저장도 가능 Answer 클래스 import 필요
     # answer = Answer(question=question, content=request.POST.get('content'), create_date=timezone.now())
-    # answer.save()
-    
+    # answer.save()QuestionForm
+
     return redirect("pybo:detail", question_id=question.id)
+
+
+def question_create(request):
+    if request.method == "POST":
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('pybo:index')
+    else:
+        form = QuestionForm()
+    context = {'form':form}
+    return render(request, 'pybo/question_form.html', context)
+
+
+    #return render(request, 'pybo/question_form.html', {"form" : form})
